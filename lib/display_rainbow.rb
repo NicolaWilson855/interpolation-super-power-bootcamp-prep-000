ace4e0de624366646918f784379d197fa7ce5010
nicola.wilson855 <nicola.wilson855@gmail.com>
2018-09-15 22:40:01 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
-def display_rainbow = (array_of_colors)
-  puts "R: #{[0]}, O: #{[1]}, Y: #{[2]}, G: #{[3]}, B: #{[4]}, I: #{[5]}, V: #{[6]} "
+def display_rainbow = (colors)
+  puts "R: #{colors[0]}, O: #{[1]}, Y: #{[2]}, G: #{[3]}, B: #{[4]}, I: #{[5]}, V: #{[6]} "
 end
